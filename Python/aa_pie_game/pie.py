9015e857dc7cdc99dbb916c5b92f11599e68f727
mikoto <ele_comb@qq.com>
2020-01-07 09:13:27 +0900

    display a title

--- Python/aa_pie_game/pie.py
+++ Python/aa_pie_game/pie.py
@@ -1,6 +1,11 @@
 import pygame
+import sys
 
 from pygame.locals import *
+
+white = 255, 255, 255
+
+blue = 0, 0, 255
 
 pygame.init()
 
@@ -8,15 +13,16 @@ screen = pygame.display.set_mode((600, 500))
 
 myfont = pygame.font.Font(None, 60)
 
-white = 255, 255, 255
-
-blue = 0, 0, 255
-
 textImage = myfont.render("Hello Pygame", True, white)
 
-screen.fill(blue)
+while True:
 
-screen.blit(textImage, (100, 100))
-
-pygame.display.update()
-
+    for event in pygame.event.get():
+        if event.type in (QUIT, KEYDOWN):
+            sys.exit()
+        
+    screen.fill(blue)
+    
+    screen.blit(textImage, (100, 100))
+    
+    pygame.display.update()
