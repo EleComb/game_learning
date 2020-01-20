0fdab81d621b5f43037d5a327fac124bb2d402ff
mikoto <ele_comb@qq.com>
2020-01-20 09:05:05 +0900

    trivia 1

---
+++ Python/test1/test.py
@@ -0,0 +1,10 @@
+from test1.test_class import Car
+from ab_trival_game.b_trivia.trivia import Trivia
+
+car = Car()
+
+print(car.width)
+
+a = Trivia("aaa")
+
+print(a)
---
+++ Python/test1/test_class.py
@@ -0,0 +1,6 @@
+
+
+class Car(object):
+    def __init__(self):
+        self.width = 200
+        self.height = 300
