b853a0d4154aa06da1df4c4e39df55bcae39d20e
mikoto <ele_comb@qq.com>
2021-04-06 12:01:27 +0900

    去掉没用的

--- Python/test1/test.py
+++
@@ -1,5 +0,0 @@
-from test1.test_class import Car
-
-car = Car()
-
-print(car.width)
--- Python/test1/test_class.py
+++
@@ -1,6 +0,0 @@
-
-
-class Car(object):
-    def __init__(self):
-        self.width = 200
-        self.height = 300
